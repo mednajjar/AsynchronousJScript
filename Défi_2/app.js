7bdc9dab79ebbc6b1feab53ec788c3e6ca8b4a0c
Mohammed Najjar <najjarmedd@gmail.com>
2020-04-15 15:43:24 +0100

    Extraire les données à partir le document JSON customer.json

--- Défi_2/app.js
+++ Défi_2/app.js
@@ -1,12 +1,30 @@
 let myJason = new XMLHttpRequest();
 let button1 = document.getElementById('button1');
+let customer = document.getElementById('customer');
 
 
-button1.addEventListener("click", function () {
-    myJason.onload = function () {
-        var ourData = JSON.parse(myJason.responseText);
-        document.getElementById('customer').textContent = myJason.responseText;
-    };
-    myJason.open('GET', 'customer.json');
-    myJason.send();
-});
+button1.addEventListener("click", loadCustomer);
+
+function loadCustomer() {
+    var ourData = JSON.parse(myJason.responseText);
+    for (p in ourData) {
+        customer.innerHTML += p + ": " + ourData[p] + "</br>";
+    }
+};
+myJason.open('GET', 'customer.json');
+myJason.send();
+//-----------------------------------------------------------------------------
+// var myJason2 = new XMLHttpRequest();
+// var button2 = document.getElementById('button2');
+// var customers = document.getElementById('customers');
+
+// button2.addEventListener("click", loadCustomers);
+
+// function loadCustomers() {
+//     var Data = JSON.parse(myJason2.responseText);
+//     for (p in Data) {
+//         customers.innerHTML += p + ": " + Data[p] + "</br>";
+//     }
+// };
+// myJason2.open('GET', 'customers.json', true);
+// myJason2.send();

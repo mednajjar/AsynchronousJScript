3e1ab4223a142749cd17a3cc92e85ad6847d8fa2
Mohammed Najjar <najjarmedd@gmail.com>
2020-04-15 14:41:33 +0100

    Update code Défi 1

--- Défi_1/app.js
+++ Défi_1/app.js
@@ -1,24 +1,23 @@
 var xhr = new XMLHttpRequest();
 var button = document.getElementById('button');
-button.onclick = function () {
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            document.getElementById("output").innerHTML = xhr.responseText;
-        }
-    };
-    xhr.open("GET", "data.txt", true);
-    xhr.send();
-}
-// button.addEventListener("click", function () {
-
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//         document.getElementById("output").textContent = xhr.responseText;
-
-
-//     }
-
+// button.onclick = function () {
+//     xhr.onreadystatechange = function () {
+//         if (this.readyState == 4 && this.status == 200) {
+//             // Typical action to be performed when the document is ready:
+//             document.getElementById("output").innerHTML = xhr.responseText;
+//         }
+//     };
 //     xhr.open("GET", "data.txt", true);
 //     xhr.send();
+// }
+button.addEventListener("click", loadData);
 
-// });
+function loadData() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        document.getElementById("output").textContent = xhr.responseText;
+    }
+
+    xhr.open("GET", "data.txt", true);
+    xhr.send();
+
+};
